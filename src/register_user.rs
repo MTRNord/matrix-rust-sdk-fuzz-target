45f21f20411505c01a9d7c0264ac9761dc18c488
MTRNord <mtrnord1@gmail.com>
2021-06-29 11:21:45 +0200

    type improvements

--- src/register_user.rs
+++ src/register_user.rs
@@ -1,18 +1,16 @@
 use matrix_sdk::api::r0::account::register::Request as RegistrationRequest;
-use matrix_sdk::api::r0::uiaa::AuthData;
 use matrix_sdk::{assign, Client};
 use tokio::runtime::Runtime;
 use url::Url;
 
 fn main() {
-    afl::fuzz!(|data: (&str, &str, &str)| {
-        let (username, password, session) = data;
+    afl::fuzz!(|data: (Option<&str>, Option<&str>)| {
+        let (username, password) = data;
         if let Ok(url) = Url::parse("http://127.0.0.1:8888") {
             if let Ok(client) = Client::new(url) {
                 let request = assign!(RegistrationRequest::new(), {
-                    username: Some(username),
-                    password: Some(password),
-                    auth: Some(AuthData::FallbackAcknowledgement { session }),
+                    username: username,
+                    password: password,
                 });
 
                 let rt = Runtime::new().unwrap();
